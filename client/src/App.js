e44ed2204c23318177d6ada05b2f0c618309cea5
GabGo51 <snow.gabo51@gmail.com>
2023-06-29 12:56:53 -0400

    creating initial routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { styled } from 'styled-components'
+import React from "react";
+import ActionBar from "./components/ActionBar";
+import Collection from "./components/Collection"
+import Profile from "./components/Profile";
 
 function App() {
   return (
-    <div>
-     yo
-    </div>
+    <Router>
+      <Container>
+      <ActionBar/>
+      <Routes>
+        <Route path="/" element={<Collection/>}/>
+        <Route path="/user" element={<Profile/>}/>
+      </Routes>
+      </Container>
+    </Router>
   );
 }
+
+const Container = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+`
 
 export default App;
---
+++ client/src/components/ActionBar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { styled } from 'styled-components'
+import { useNavigate } from 'react-router-dom'
+
+const ActionBar = () => {
+
+  const navigate = useNavigate()
+  
+  return (
+    <Container>
+      <button onClick={()=>navigate("/")}>PLANT</button>
+      <button onClick={()=>navigate("/user")}>PROFILE</button>
+    </Container>
+  )
+}
+
+
+const Container = styled.div`
+display: flex;
+justify-content: space-between;
+position: absolute;
+width: 80vw;
+bottom: 0;
+`
+export default ActionBar
