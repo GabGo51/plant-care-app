fb45cf5dbc943341cd9ee3ccbcc6941f4faa7de1
GabGo51 <snow.gabo51@gmail.com>
2023-07-18 15:09:36 -0400

    gg

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -37,7 +37,7 @@ const Login = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    fetch(`${process.env.RENDER}/api/signin`, {
+    fetch(`https://plant-care-app.onrender.com/api/signin`, {
       method: "POST",
       headers: {
         Accept: "application/json",
